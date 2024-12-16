a95ea98e6783ce35c2043e07c6bbdf31a72102a8
QMoayedpour <quentin.moayedpour@ensae.fr>
2024-12-16 18:35:12 +0000

    upd requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="PGM_sujet_3",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["networkx", "torch", "numpy",
+                      "matplotlib", "scikit-learn", "scipy"],
 )
